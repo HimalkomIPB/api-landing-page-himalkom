d6e9b7c0b1c229928f06c3d3c5e1e0007a9567df
AghnatHs <aghnathasya@gmail.com>
2025-03-13 18:47:08 +0700

    Add division relation to DivisionWorkProgram

--- app/Models/DivisionWorkProgram.php
+++ app/Models/DivisionWorkProgram.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DivisionWorkProgram extends Model
 {
     protected $guarded = ['id'];
+
+    public function division(): BelongsTo
+    {
+        return $this->belongsTo(Division::class);
+    }
 }
