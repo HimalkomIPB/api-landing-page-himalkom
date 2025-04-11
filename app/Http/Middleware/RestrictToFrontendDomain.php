f22ed91196d7d310804c42c48ce7f706b0e24266
AghnatHs <aghnathasya@gmail.com>
2025-04-11 11:10:32 +0700

    Feat: restrict API access to frontend url only

---
+++ app/Http/Middleware/RestrictToFrontendDomain.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RestrictToFrontendDomain
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (app()->environment('local', 'development')) {
+            return $next($request);
+        }
+
+        $allowedDomain = env('ALLOWED_ORIGIN');
+
+        $origin = $request->headers->get('Origin');
+        $referer = $request->headers->get('Referer');
+
+        if (
+            ($origin && parse_url($origin, PHP_URL_HOST) !== $allowedDomain) &&
+            ($referer && parse_url($referer, PHP_URL_HOST) !== $allowedDomain)
+        ) {
+            return response()->json(['errors' => 'Forbidden'], 403);
+        }
+
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: ''
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        \App\Http\Middleware\RestrictToFrontendDomain::class;
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
