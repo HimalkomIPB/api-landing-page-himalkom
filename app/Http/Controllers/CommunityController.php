b2f34b3d1c436c59a04bf85668f70aec48d98aec
AghnatHs <aghnathasya@gmail.com>
2025-05-21 21:47:03 +0700

    Feat: CommunityPortofolio controller and routes

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Community;
+use App\Models\CommunityPortofolio;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -27,6 +28,19 @@ class CommunityController extends Controller
             return response()->json(['community' => $community]);
         } catch (ModelNotFoundException) {
             return response()->json(['errors' => 'Community not found'], 404);
+        }
+    }
+
+    public function indexPortofolio(string $slug): JsonResponse
+    {
+        try {
+            $communityPortofolios = CommunityPortofolio::whereHas('community', function ($query) use ($slug) {
+                $query->where('slug', $slug);
+            })->get();
+
+            return response()->json(['communityPortofolios' => $communityPortofolios]);
+        } catch (ModelNotFoundException) {
+            return response()->json(['errors' => 'Community not found or has no portfolios'], 404);
         }
     }
 }
